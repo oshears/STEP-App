8992a37534fefa88b173f9a9401fa204143130d6
Osaze Shears <yoze15@gmail.com>
2015-03-15 19:29:54 -0400

    New Calendar Icon Concept

--- STEP App/AdminTableViewController.swift
+++ STEP App/AdminTableViewController.swift
@@ -48,19 +48,7 @@ class AdminTableViewController: UITableViewController {
         // Dispose of any resources that can be recreated.
     }
 
-    // MARK: - Table view data source
 
-    override func numberOfSectionsInTableView(tableView: UITableView) -> Int {
-        // #warning Potentially incomplete method implementation.
-        // Return the number of sections.
-        return 1
-    }
-
-    override func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        // #warning Incomplete method implementation.
-        // Return the number of rows in the section.
-        return 3
-    }
     override func tableView(tableView: UITableView, didSelectRowAtIndexPath indexPath: NSIndexPath) {
         tableView.deselectRowAtIndexPath(indexPath, animated: true)
         if indexPath.row == 0{
---
+++ STEP App/CalendarTableViewCell.swift
@@ -0,0 +1,30 @@
+//
+//  CalendarTableViewCell.swift
+//  STEP App
+//
+//  Created by Osaze Shears on 3/15/15.
+//  Copyright (c) 2015 OSApps. All rights reserved.
+//
+
+import UIKit
+
+class CalendarTableViewCell: UITableViewCell {
+
+    @IBOutlet weak var monthLabel: UILabel!
+    @IBOutlet weak var dayLabel: UILabel!
+    @IBOutlet weak var mainActivity1: UILabel!
+    @IBOutlet weak var mainActivity2: UILabel!
+    @IBOutlet weak var mainActivity3: UILabel!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
